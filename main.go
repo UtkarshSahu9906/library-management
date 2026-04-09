82968a1a7a6cf90c532f53f1e2f443e766ad5b20
UtkarshSahu9906 <UtkarshSahu9906@gmail.com>
2026-04-09 16:38:07 +0530

    add basic server -port

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Library Management System starting...")
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Welcome to Library Management System")
+	})
+
+	log.Println("Server starting on port 9090...")
+	err := http.ListenAndServe(":9090", mux)
+	if err != nil {
+		log.Fatal("Server failed to start: ", err)
+	}
 }
