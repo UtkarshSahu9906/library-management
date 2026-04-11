ce136237956660879d42f0fd45b5d66ad827dc90
UtkarshSahu9906 <UtkarshSahu9906@gmail.com>
2026-04-11 18:25:47 +0530

    add POST/borrow - issue book to member

--- handlers.go
+++ handlers.go
@@ -2,8 +2,76 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
 )
+
+func borrowBook(w http.ResponseWriter, r *http.Request) {
+	var request struct {
+		BookID   string `json:"book_id"`
+		MemberID string `json:"member_id"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&request)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	bookIndex := -1
+	for i, b := range books {
+		if b.ID == request.BookID {
+			bookIndex = i
+			break
+		}
+	}
+
+	if bookIndex == -1 {
+		http.Error(w, "Book not found", http.StatusNotFound)
+		return
+	}
+
+	if !books[bookIndex].Available {
+		http.Error(w, "Book is not available", http.StatusConflict)
+		return
+	}
+
+	memberFound := false
+
+	for _, m := range members {
+		if m.ID == request.MemberID {
+			memberFound = true
+			break
+		}
+	}
+
+	if !memberFound {
+		http.Error(w, "Member not found", http.StatusNotFound)
+		return
+	}
+
+	books[bookIndex].Available = false
+
+	borrowDate := time.Now()
+	dueDate := borrowDate.AddDate(0, 0, 14)
+	record := BorrowRecord{
+		ID:         fmt.Sprintf("BR%d", len(borrowRecords)+1),
+		BookID:     request.BookID,
+		MemberID:   request.MemberID,
+		BorrowDate: borrowDate.Format("2006-01-02"),
+		DueDate:    dueDate.Format("2006-01-02"),
+		ReturnDate: "",
+		Fine:       0,
+		Returned:   false,
+	}
+
+	borrowRecords = append(borrowRecords, record)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(record)
+
+}
 
 // getAllBooks handles GET /books
 func getAllBooks(w http.ResponseWriter, r *http.Request) {
--- main.go
+++ main.go
@@ -11,18 +11,29 @@ func main() {
 
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
-    switch r.Method {
-    case http.MethodGet:
-        getAllBooks(w, r)
-    case http.MethodPost:
-        addBook(w, r)
-    default:
-        http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-    }
-})
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Welcome to Library Management System")
+	})
 
-	//mux.HandleFunc("/books", getAllBooks)
+	mux.HandleFunc("/books", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			getAllBooks(w, r)
+		case http.MethodPost:
+			addBook(w, r)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
+	mux.HandleFunc("/borrow", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodPost:
+			borrowBook(w, r)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
 
 	port := ":8080"
 	log.Println("Server starting on port", port)
